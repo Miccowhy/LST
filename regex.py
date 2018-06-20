9461ca5dc8574a94cbbf408ecff887dd46221c6b
Mikołaj Aleksandrzak <37934103+Miccowhy@users.noreply.github.com>
2018-06-20 13:40:07 +0200

    Add files via upload

--- regex.py
+++ regex.py
@@ -3,53 +3,60 @@ import re
 operations = {'N0': '1', 'N1': '0', 'A00': '0', 'A01': '1', 'A10': '1', 'A11': '1', 'K00': '0', 'K01': '0', 'K10': '0', 'K11': '0', 'C00': '1', 'C01': '1', 'C10': '0', 'C11': '1', 'E00': '1', 'E01': '0', 'E10': '0', 'E11': '1'}
 
 regex = r"([AKCE][p][\d]*[p][\d]*|[N][p][\d]*)"
-regex_forbidden = r"([p][0][\d]*|[BDF-JL-MO-Za-or-z_]|[\W])"
+regex_forbidden = r"([p][0][\d]*|[BDF-JL-MO-Za-oq-z_]|[\W])"
 regex_variables = r"([p][\d]*)"
 
 def printer(truth_dict, var_list):
     truth_table = sorted(truth_dict, reverse=True)
+    no_false = True
 
-    print("False for:\n")
+    output = "False for:\n"
     for x in var_list:
-        print(x, end=' ')
-    print()
+        output += x + ' '
+    output += "\n"
 
     for var in truth_table:
         if truth_dict[var] == '0':
+            no_false = False
             for x in range(len(var)):
-                print(var[x] + ' ' * len(var_list[x]), end='')
-            print()
+                output += var[x] + ' ' * len(var_list[x])
+            output += "\n"
 
-def solver(var_value, var_list, function):
+    if no_false == False:
+        print(output, end = '')
+    else:
+        print("No occurences where formula is false")
+
+def solver(var_value, var_list, formula):
     for x in range(len(var_list)-1, -1, -1):
-        function = function.replace(var_list[x], var_value[x])
-    while len(function) != 1:
+        formula = formula.replace(var_list[x], var_value[x])
+    while len(formula) != 1:
         for op, val in operations.items():
-            function = function.replace(op, val)
-    return function
+            formula = formula.replace(op, val)
+    return formula
 
-def manipulator(function):
-    var_list = sorted(set(re.findall(regex_variables, function)))
+def manipulator(formula):
+    var_list = sorted(set(re.findall(regex_variables, formula)))
     var_amount = len(var_list)
     power = 2**var_amount
     truth_dict = dict()
 
     for x in range(power):
         var_value = bin(x)[2:].zfill(var_amount)
-        truth_dict[var_value] = solver(var_value, var_list, function)
+        truth_dict[var_value] = solver(var_value, var_list, formula)
     printer(truth_dict, var_list)
 
-def syntax():
-    function = checker = input()
+def syntax(formula):
+    checker = formula
 
     validity = True
     i = 0
 
     forbidden = re.findall(regex_forbidden, checker)
     if forbidden == []:
-        single_var = re.findall(regex_variables, function)
-        if function == single_var[0]:
-            print("False for:\n" + function + "\n0")
+        single_var = re.findall(regex_variables, formula)
+        if formula == single_var[0]:
+            print("False for:\n" + formula + "\n0")
         else:
             while checker != "p0":
                 parser = re.findall(regex, checker)
@@ -63,10 +70,11 @@ def syntax():
                     checker = checker.replace(x, "p0", 1)
                 i += 1
             if validity == True:
-                manipulator(function)
+                manipulator(formula)
     else:
         print("Non-valid characters used")
 
 while True:
-    syntax()
+    formula = input()
+    syntax(formula)
     print("-----------------------------------------")
